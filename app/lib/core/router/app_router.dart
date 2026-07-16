223b579280e228c2a6250122ba97bcd872b30d78
Leonardo de Souza Guimarães <leozinhoszg@gmail.com>
2026-07-16 06:48:55 -0300

    feat(app): navegação de raids e progressão a partir da Home

--- app/lib/core/router/app_router.dart
+++ app/lib/core/router/app_router.dart
@@ -6,6 +6,10 @@ import '../../features/home/home_screen.dart';
 import '../../features/characters/characters_list_screen.dart';
 import '../../features/characters/character_form_screen.dart';
 import '../../features/characters/character_profile_screen.dart';
+import '../../features/characters/character_progression_screen.dart';
+import '../../features/raids/raids_list_screen.dart';
+import '../../features/raids/raid_form_screen.dart';
+import '../../features/raids/raid_detail_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
@@ -27,6 +31,13 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: '/characters/:id',
         builder: (_, state) => CharacterProfileScreen(id: int.parse(state.pathParameters['id']!)),
       ),
+      GoRoute(
+        path: '/characters/:id/progression',
+        builder: (_, state) => CharacterProgressionScreen(id: int.parse(state.pathParameters['id']!)),
+      ),
+      GoRoute(path: '/raids', builder: (_, _) => const RaidsListScreen()),
+      GoRoute(path: '/raids/new', builder: (_, _) => const RaidFormScreen()),
+      GoRoute(path: '/raids/:id', builder: (_, state) => RaidDetailScreen(id: int.parse(state.pathParameters['id']!))),
     ],
   );
 });
