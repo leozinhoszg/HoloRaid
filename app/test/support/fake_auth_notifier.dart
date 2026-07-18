a41612fc2ef00407c3d2ecd730a46c8a23b2568a
Leonardo de Souza Guimarães <leozinhoszg@gmail.com>
2026-07-17 21:05:10 -0300

    chore(app): super parameter no FakeAuthNotifier (lint)

--- app/test/support/fake_auth_notifier.dart
+++ app/test/support/fake_auth_notifier.dart
@@ -1,8 +1,7 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:holoraid/core/auth/auth_providers.dart';
 
 class FakeAuthNotifier extends AuthNotifier {
-  FakeAuthNotifier(Ref ref, AuthState initial) : super(ref) {
+  FakeAuthNotifier(super.ref, AuthState initial) {
     state = initial;
   }
 }
